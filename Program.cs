1b4831b9d8d28a32e0e2b9d4fff55420c7ad2f02
toll8030 <33236055+toll8030@users.noreply.github.com>
2019-01-28 11:22:55 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-using system.net;
+using System.Net;
+using System.Net.Sockets;
 namespace TinyWebServer_v1._0
 {
+    class Server
+    {
+        private int port;
+        
+
+
+
+
+
     class Program
     {
         static void Main(string[] args)
